03ab9cdc75534f4d21463df5c03df3d293a84e8f
Hongyu Zhu <serailhydra@gmail.com>
2020-07-24 15:54:59 +0800

    Update run_large.sh

--- BERT/run_large.sh
+++ BERT/run_large.sh
@@ -66,7 +66,6 @@ fi
 $PREFIX run_squad.py \
   --bert_model bert-large-uncased \
   --do_train \
-  --do_predict \
   --do_lower_case \
   --train_file train-v1.1.json \
   --predict_file dev-v1.1.json \
