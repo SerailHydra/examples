547226ff28032d4dab1dbf26e0b5f8b8276d79ae
Hongyu Zhu <serailhydra@gmail.com>
2020-07-31 12:59:32 +0800

    Update run.sh

--- mnist/run.sh
+++ mnist/run.sh
@@ -3,11 +3,15 @@ SUFFIX='--epochs 1'
 
 for var in "$@"
 do
+    if [ $var = "cupti" ]; then
+        SUFFIX="${SUFFIX} --cupti"
+    fi
     if [ $var = "nsight" ]; then
+        SUFFIX="${SUFFIX} --nsight"
         PREFIX="/opt/nvidia/nsight-systems/2020.3.1/bin/nsys profile ${PREFIX}"
     fi
 done
 
-CUDA_VISIBLE_DEVICES=3
+CUDA_VISIBLE_DEVICES=1
 
 $PREFIX main.py $SUFFIX
